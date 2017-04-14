73b3416f33c0d3f31aa06a58148253f495985ec5
Kees van der vlis <keesvandervlis@myprecious.slowthikers.net>
2017-04-14 09:42:10 +0200

    Initial Commit

---
+++ swiftappno1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftappno1
+//
+//  Created by Kees van der vlis on 14/04/2017.
+//  Copyright © 2017 Kees van der vlis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
